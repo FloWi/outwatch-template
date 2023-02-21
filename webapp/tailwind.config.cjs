dd6ecc80a09b9b3cad9254a839c02f74147de0e0
Florian Witteler <github@flwi.de>
2023-02-21 12:33:26 +0100

    tailwind meets vite meets outwatch

--- webapp/tailwind.config.cjs
+++ webapp/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['../../../src/main/html/index.html', '../../../src/**/*.scala'],
+  content: ['src/main/html/index.html', 'src/**/*.scala'],
   // mode: 'jit',
   variants: {
     extend: {},
--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from "vite-plugin-top-level-await"
 
-
 function isDev() {
   return process.env.NODE_ENV !== "production";
 }
